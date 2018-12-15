41030636d7a9fdab5a5fb45d2392c26cf82182d1
Tommy Ryanto <tommyryanto@icloud.com>
2018-12-15 23:41:54 +0700

    updated v2 server.js

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ app.listen(port, function(error) {
   }
 })
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || port, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
